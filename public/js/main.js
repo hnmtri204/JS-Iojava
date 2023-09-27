a7244b41edd6b2c0bd1a6cf9cf3df0cdb1c660fb
hnmtri204 <120084560+hnmtri204@users.noreply.github.com>
2023-09-27 08:20:43 +0700

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+/*Thông tin sinh viên 1*/
+var ten1 = "Ngô Ngọc Thắng <br>"; 
+var masinhvien1 = "2260178 <br>";
+var CCCD1 ="04620632874 <br>";
+var tenlop1 = "22CDTH41 <br>";
+var tentruong1 =" CĐCN Huế <br>";
+var điemtb1 ="10đ <br>";
+document.writeln ("thông tin sinh viên 1: <br><br> "+ "tên: " + ten1 + "mã sinh viên: "+ masinhvien1 + "CCCD: "+CCCD1 +"tên lớp: "+ tenlop1 +"tên trường: "+ tentruong1 +"điểm tb: "+ điemtb1 + "<br>");
+/*Thông tin công nhân 1*/
+var ten11 = "Trần Văn A  <br>";  
+var diachi11 ="70 Nguyễn Huệ <br>"
+var sdt11 = "0682781135 <br>";
+var hesoluong11 ="3.000.000 <br>"; 
+document.writeln ("thông tin công nhân 1 <br><br> "+ "tên: " + ten11 + "địa chỉ: " + diachi11 + "Sđt: " + sdt11 + "Hệ số lương: " +hesoluong11+ "<br>");
+/*Thông tin chiếc xe car 1*/
+var ten = "A <br>";
+var hang = "B <br>"; 
+var gia = "6.000.000 <br>"
+var mauson = "xanh <br>";
+document.writeln ("thông tin chiếc xe car 1 <br><br> "+ "tên: " + ten + "hảng: " + hang + "giá: " +gia + "màu sơn: " + mauson + "<br>");
+/*Thông tin sinh viên 2*/
+var ten2 = "Hoàng Ngọc Minh Trí <br>"
+var masinhvien2 = "2260178 <br>";
+var CCCD2 ="04620632874 <br>";
+var tenlop2 = "22CDTH41 <br>";
+var tentruong2 =" CĐCN Huế <br>";
+var điemtb2 ="10đ <br>";
+document.writeln ("thông tin sinh viên 2 <br><br> "+ "tên: " + ten2 + "mã sinh viên: "+masinhvien2 + "CCCD: "+ CCCD2 + "tên lớp: "+ tenlop2 + "tên trường: "+ tentruong2 + "điểm tb: "+ điemtb2 + "<br>");
+/*Thông tin công nhân 2*/
+var ten22 = "Trần Văn B <br>";  
+var diachi22 ="70 Nguyễn Huệ <br>"
+var sdt22 = "0682781135 <br>";
+var hesoluong22 ="3.000.000 <br>"
+document.writeln ("thông tin công nhân2 <br><br> "+ "tên: "+ ten22 + "địa chỉ: " +  diachi22 + "Sđt: " +sdt22 + "Hệ số lương: " + hesoluong222+ "<br>");
+/*Thông tin chiếc xe car 2*/
+var ten222 = "C <br>";
+var hang222 = "D <br>"; 
+var gia222 = "6.000.000 <br>"
+var mauson222 = "xanh <br>";
+document.writeln ("thông tin chiếc xe car2 <br><br> "+ "tên: " + ten222 + "hảng: "+ hang222 + "giá: " + gia222 +  "màu sơn: " +mauson222 + "<br>");
